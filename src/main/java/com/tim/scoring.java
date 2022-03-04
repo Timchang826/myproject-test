8014013e27854aa500f538f3ad1af2f38e772e2e
User <timchang0001@gmail.com>
2022-03-04 15:26:58 +0800

    commit scoring

---
+++ src/main/java/com/tim/scoring.java
@@ -0,0 +1,14 @@
+package com.tim;
+
+public class scoring {
+    public static void main(String[] args) {
+        String[] names={"a","b","c","d","e"};
+        int[] eng={100,90,80,70,60};
+        int[] math={80,70,60,50,40};
+        for (int i = 0; i < 5; i++) {
+            int s;
+            s=(math[i]+eng[i])/2;
+            System.out.println(names[i]+"\t"+eng[i]+"\t"+math[i]+"\t"+s);
+        }
+    }
+}
