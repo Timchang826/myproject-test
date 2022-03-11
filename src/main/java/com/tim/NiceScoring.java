8badb8a6b943b86a91e7253ff29f753bc5c73616
User <timchnag0001@gmail.com>
2022-03-11 15:06:47 +0800

    initial commit

--- src/main/java/com/tim/NiceScoring.java
+++ src/main/java/com/tim/NiceScoring.java
@@ -2,9 +2,14 @@ package com.tim;
 
 public class NiceScoring {
     public static void main(String[] args) {
-        Student jack=new Student();
-        jack.name="Jack";
+        Student jack=new Student("jack");
+//        jack.name="Jack";
         jack.eng=100;
         jack.math=90;
+        Student timmy=new Student("timmy",100,100);
+        Student s = new Student();
+        jack.print();
+        timmy.print();
+        s.print();
     }
 }
--- src/main/java/com/tim/Student.java
+++ src/main/java/com/tim/Student.java
@@ -3,5 +3,22 @@ package com.tim;
 public class Student {
     String name;
     int eng,math;
+    public Student(String name){
+        this.name=name;
+    }
+    public Student(String name,int eng,int math){
+        this(name);
+        this.eng=eng;
+        this.math=math;
+    }
+    public Student(){
+        this("john doe",-1,-1);
+ //       name = "john doe";
+ //       eng = -1;
+ //       math = -1;
+    }
+    public void print(){
+        System.out.println(name + "\t" + eng + "\t" + math);
+    }
 
 }
