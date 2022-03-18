993e572659ca96b922eb118541cb7663b3b918f2
User <timchang0002@gamil.com>
2022-03-18 15:11:38 +0800

    Commit Shop2

--- src/main/java/com/tim/Shop/Shop.java
+++ src/main/java/com/tim/Shop/Shop.java
@@ -3,7 +3,7 @@ package com.tim.Shop;
 public class Shop {
     public static void main(String[] args) {
         Vipnormal one = new Vipnormal("0001",1000);
-        Vipnormal two = new Vipnormal("0002",800);
+        Vipnormal two = new Vipnormal("0002",900);
         VipAg three = new VipAg("0003", 2000);
         one.print();
         two.print();
